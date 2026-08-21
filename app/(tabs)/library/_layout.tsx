45409a01fac148d8297f28f4269e2831707b48fd
Adithya Athreya <adithyaathreya2005@gmail.com>
2026-08-21 17:17:51 +0530

    wraps library routes in a stack navigator

---
+++ app/(tabs)/library/_layout.tsx
@@ -0,0 +1,10 @@
+import { Stack } from 'expo-router';
+import React from 'react';
+import { useTheme } from '../../../src/ui/theme/ThemeProvider';
+
+export default function LibraryLayout() {
+  const { tokens } = useTheme();
+  return (
+    <Stack screenOptions={{ headerShown: false, contentStyle: { backgroundColor: tokens.background } }} />
+  );
+}
